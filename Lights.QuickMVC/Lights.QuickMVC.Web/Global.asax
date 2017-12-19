00338e2db8130d037c4f904861ee960548885edf
xiaochao <chao.xiao@kingsun.cn>
2017-12-19 09:54:51 +0800

    初始状态

---
+++ Lights.QuickMVC/Lights.QuickMVC.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lights.QuickMVC.Web.MvcApplication" Language="C#" %>
---
+++ Lights.QuickMVC/Lights.QuickMVC.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lights.QuickMVC.Web.Startup))]
+namespace Lights.QuickMVC.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
