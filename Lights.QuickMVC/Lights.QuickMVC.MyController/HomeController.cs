a8d9580405e96f19fcf08361262229a0e527a295
DESKTOP-LGJ01GD\Administrator <xiaochao310@foxmail.com>
2017-12-19 20:40:24 +0800

    提交一个丢失的页面

---
+++ Lights.QuickMVC/Lights.QuickMVC.MyController/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Lights.QuickMVC.MyModel;
+
+namespace Lights.QuickMVC.MyController
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ModelDemo model = new ModelDemo
+            {
+                id = 1,
+                name = "肖超"
+            };
+            return View(model);
+        }
+    }
+}
