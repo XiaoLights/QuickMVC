5e6aa802d9dbf7af4ec9d3bd4af500501d8490eb
DESKTOP-LGJ01GD\Administrator <xiaochao310@foxmail.com>
2017-12-21 19:30:05 +0800

    123

---
+++ Lights.QuickMVC/Lights.Framework.Manager/SqlSugarCreateEntity.cs
@@ -0,0 +1,22 @@
+﻿using Lights.Framework.SqlSugar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lights.Framework.Manager
+{
+    public class SqlSugarCreateEntity
+    {
+        SqlsugarRepository repository = new SqlsugarRepository();
+
+        public void CreateEntity()
+        {
+            using (var db = repository.GetInstance())
+            {
+                db.DbFirst.CreateClassFile("E:\\GitProject\\QuickMVC\\EntityFile");
+            }
+        }
+    }
+}
